963c9cd175dda4dc228f2a7c93c9d5ea37d84ee1
maheini <maheini35@me.com>
2023-01-05 18:33:39 +0100

    plugin content

--- woocommerce_surpress_completed_email_for_digital_products.php
+++ woocommerce_surpress_completed_email_for_digital_products.php
@@ -8,5 +8,25 @@
  * Author URI: http://neofix.ch
  */
 
+class NeofixWooSurpressDigitalCompletedMail{
+  function __construct(){
+    add_filter( 'woocommerce_email_recipient_customer_completed_order', array( $this, 'conditional_email_on_completed_order' ), 10, 2 );
+  }
+
+  function conditional_email_on_completed_order( $recipient, $order ) {
+    $items = $order->get_items();
+    foreach ( $items as $item ) {
+      $product = wc_get_product($item->get_product_id());
+      if (!$product->is_virtual()) {
+        return $recipient;
+      }
+    }
+    return '';
+  }
+}
+
+if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
+  new NeofixWooSurpressDigitalCompletedMail(); 
+}
 
 ?>

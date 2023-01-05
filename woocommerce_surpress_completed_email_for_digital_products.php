fc1e69c4c8354914e8697917cb5d59709b7b5d93
maheini <maheini35@me.com>
2023-01-05 18:32:48 +0100

    initial commit

---
+++ woocommerce_surpress_completed_email_for_digital_products.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: Woocommerce surpress completed email for digital products
+ * Plugin URI: http://neofix.ch/
+ * Description: Woocommerce Plugin to surpress completed email for digital products.
+ * Version: 1.0.0
+ * Author: Neofix
+ * Author URI: http://neofix.ch
+ */
+
+
+?>
